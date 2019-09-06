e61c4757985f2600b920d3185552145bedd389d9
Damian Legawiec <damian@sparksolutions.co>
2019-09-06 11:45:00 +0200

    Don't include webacker

--- lib/language_pack/spree.rb
+++ lib/language_pack/spree.rb
@@ -13,7 +13,7 @@ class LanguagePack::Spree < LanguagePack::Rails6
     run_command 'gem install --user-install --no-ri --no-rdoc bundler'
 
     rails_path = `ruby -e "gem 'railties'; puts Gem.bin_path('railties', 'rails')"`.strip
-    run_command "#{rails_path} new sandbox --skip-bundle --database=postgresql"
+    run_command "#{rails_path} new sandbox --skip-bundle --database=postgresql --skip-javascript --skip-coffee"
 
     run_command "cp -rf sandbox/* ."
     run_command "rm -rf sandbox"
