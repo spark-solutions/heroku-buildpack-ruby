3a8ca958129761c5ca1fd732db5499d925dcde70
Szymon Iwacz <szymon@iwacz.pl>
2020-10-02 13:13:13 +0200

    Bump node version to 14.13.0

--- lib/language_pack/helpers/nodebin.rb
+++ lib/language_pack/helpers/nodebin.rb
@@ -11,7 +11,7 @@ class LanguagePack::Helpers::Nodebin
   end
 
   def self.hardcoded_node_lts
-    version = "8.10.0"
+    version = "14.13.0"
     {
       "number" => version,
       "url"    => "https://s3pository.heroku.com/node/v#{version}/node-v#{version}-linux-x64.tar.gz"
